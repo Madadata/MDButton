7322447cea9ea5b85302605296216b48ec90ff87
zhenghe <zhenghe@madadata.com>
2016-07-30 11:58:59 +0800

    modify lint method

--- test/MDButton.spec.js
+++ test/MDButton.spec.js
@@ -1,14 +1,14 @@
+/* eslint-disable no-undef */
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 
 import React from 'react';
 import { createRenderer } from 'react-addons-test-utils';
-import MDButton from 'src/MDButton';
+import MDButton from 'src/MDButton'; // eslint-disable-line import/no-unresolved
 
 expect.extend(expectJSX);
 
 describe('MDButton', () => {
-
   it('should render a button with text as children', () => {
     const renderer = createRenderer();
     renderer.render(
@@ -18,12 +18,12 @@ describe('MDButton', () => {
     );
     const actualElement = renderer.getRenderOutput();
     expect(actualElement.type).toEqual('button');
-    expect(actualElement.props.children).toEqual('text')
+    expect(actualElement.props.children).toEqual('text');
   });
 
   it('should fire action function if specified', () => {
     let clicked = false;
-    const onClick = () => { clicked = true };
+    const onClick = () => { clicked = true; };
     const renderer = createRenderer();
     renderer.render(
       <MDButton
@@ -51,7 +51,7 @@ describe('MDButton', () => {
     renderer.render(<MDButton />);
     const actualElement = renderer.getRenderOutput();
     expect(actualElement.props.type).toEqual('button');
-  })
+  });
 
   it('should render a typed button if type is specified', () => {
     const renderer = createRenderer();
@@ -62,7 +62,7 @@ describe('MDButton', () => {
     );
     const actualElement = renderer.getRenderOutput();
     expect(actualElement.props.type).toEqual('submit');
-  })
+  });
 
   it('should render a disabled button if isDisabled is true', () => {
     const renderer = createRenderer();
@@ -73,7 +73,7 @@ describe('MDButton', () => {
     );
     const actualElement = renderer.getRenderOutput();
     expect(actualElement.props.disabled).toBe(true);
-  })
+  });
 
   it('should render a button with given width and height', () => {
     const renderer = createRenderer();
@@ -82,9 +82,9 @@ describe('MDButton', () => {
         height={100}
         width={200}
       />
-  );
+    );
 
-  const actualElement = renderer.getRenderOutput();
-  expect(actualElement.props.style).toInclude({ width: 200, height: 100 });
+    const actualElement = renderer.getRenderOutput();
+    expect(actualElement.props.style).toInclude({ width: 200, height: 100 });
   });
 });
